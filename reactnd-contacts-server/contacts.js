6ca869e1d4faec9452cc614b63e20ae0e3e24607
Aishwarya Murahari <aishwarya26ramagiri@gmail.com>
2023-10-12 17:09:10 -0700

    updated

--- reactnd-contacts-server/contacts.js
+++ reactnd-contacts-server/contacts.js
@@ -1,7 +1,7 @@
-const clone = require('clone')
-const config = require('./config')
+const clone = require('clone');
+const config = require('./config');
 
-const db = {}
+const db = {};
 
 const defaultData = {
   contacts: [
@@ -9,57 +9,63 @@ const defaultData = {
       id: '242dfef8-529a-11eb-ae93-0242ac130002',
       name: 'Ryan Florence',
       email: 'ryan@reacttraining.com',
-      avatarURL: config.origin + '/ryan.jpg'
+      avatarURL: config.origin + '/ryan.jpg',
     },
     {
       id: '299992c6-529a-11eb-ae93-0242ac130002',
       name: 'Michael Jackson',
       email: 'michael@reacttraining.com',
-      avatarURL: config.origin + '/michael.jpg'
+      avatarURL: config.origin + '/michael.jpg',
     },
     {
       id: '307f0b0c-529a-11eb-ae93-0242ac130002',
       name: 'Tyler McGinnis',
       email: 'tyler@reacttraining.com',
-      avatarURL: config.origin + '/tyler.jpg'
-    }
-  ]
-}
+      avatarURL: config.origin + '/tyler.jpg',
+    },
+    {
+      id: '307f0b0c-529a-11eb-ae93-0242ac1300322',
+      name: 'Aish mur',
+      email: 'aish@reacttraining.com',
+      avatarURL: config.origin + '/tyler.jpg',
+    },
+  ],
+};
 
 const get = (token) => {
-  let data = db[token]
+  let data = db[token];
 
   if (data == null) {
-    data = db[token] = clone(defaultData)
+    data = db[token] = clone(defaultData);
   }
 
-  return data
-}
+  return data;
+};
 
 const add = (token, contact) => {
   if (!contact.id) {
-    contact.id = Math.random().toString(36).substr(-8)
+    contact.id = Math.random().toString(36).substr(-8);
   }
 
-  get(token).contacts.push(contact)
+  get(token).contacts.push(contact);
 
-  return contact
-}
+  return contact;
+};
 
 const remove = (token, id) => {
-  const data = get(token)
-  const contact = data.contacts.find(c => c.id === id)
+  const data = get(token);
+  const contact = data.contacts.find((c) => c.id === id);
 
   if (contact) {
-    data.contacts = data.contacts.filter(c => c !== contact)
+    data.contacts = data.contacts.filter((c) => c !== contact);
   }
 
-  return { contact }
-}
+  return { contact };
+};
 
 module.exports = {
   get,
   add,
   remove,
-  defaultData
-}
+  defaultData,
+};
